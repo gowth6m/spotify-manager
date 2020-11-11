98691b007b23554b84ee71f7443f352707c0eaee
Gowtham <gowtham4n@gmail.com>
2020-11-11 03:37:32 +0000

    Init

---
+++ main.py
@@ -0,0 +1,12 @@
+class stalker:
+
+    def __init__(self,name):
+        self.name = name
+
+    def tester(self):
+        print("tester")
+
+
+if __name__ == '__main__':
+    stalker = stalker("Test Stalker")
+    stalker.tester()
